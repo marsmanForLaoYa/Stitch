8f4e0766ce9d1abd4f6d0707acde6e4b03fab9b3
mac_m11 <>
2022-10-13 09:54:25 +0800

    test

---
+++ YXStitch/Class/Login/ss.h
@@ -0,0 +1,16 @@
+//
+//  ss.h
+//  YXStitch
+//
+//  Created by mac_m11 on 2022/10/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ss : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
